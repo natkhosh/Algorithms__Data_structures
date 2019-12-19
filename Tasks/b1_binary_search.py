961ec060006897a9fca0e7205eac63564f90c12a
natkhosh <t9219788607@gmail.com>
2019-12-19 20:27:56 +0300

    b1 - выполнено и проверенно тестом!!!! УРАААААА

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,4 +1,6 @@
 from typing import Any, Sequence, Optional
+import numpy as np
+import random
 
 
 def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
@@ -10,5 +12,38 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
 
-	print(elem, arr)
-	return None
+	index = None
+	left = 0
+	right = len(arr) - 1
+
+	while right > left + 1:
+		middle = (left + right) // 2
+		if arr[middle] == elem:
+			index = middle
+			break
+		elif arr[middle] > elem:
+			right = middle
+		else:
+			left = middle
+	else:
+		if elem == arr[right]:
+			index = right
+		elif elem == arr[left]:
+			index = left
+		else:
+			index = None
+
+	return index
+
+
+	#print(elem, arr)
+	# return None
+
+
+if __name__== "__main__":
+	n = 10
+	array = np.arange(n)
+	element = 11
+	#element = np.random.choice(array)
+	print(array, element)
+	print(binary_search(element, array))
