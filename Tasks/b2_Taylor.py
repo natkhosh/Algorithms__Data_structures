e4596f45630005de7ffb106aee2c92412ee87f2a
natkhosh <t9219788607@gmail.com>
2019-12-19 21:20:23 +0300

    b1 - выполнено и проверенно тестом!!!! УРАААААА

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,17 +2,30 @@
 Taylor series
 """
 from typing import Union
+import math
 
-
-def ex(x: Union[int, float]) -> float:
+def ex(x: Union[int, float], delta=0.0001) -> float:
 	"""
 	Calculate value of e^x with Taylor series
 
 	:param x: x value
 	:return: e^x value
 	"""
+	if x == 0:
+		return 1
+	else:
+		result_sum = 1
+		n = 1
+		while True:
+			result = (x ** n) / math.factorial(n)
+			if result < delta:
+				return result_sum
+			else:
+				result_sum += result
+				n += 1
+
 	print(x)
-	return 0
+
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -24,3 +37,5 @@ def sinx(x: Union[int, float]) -> float:
 	"""
 	print(x)
 	return 0
+
+
