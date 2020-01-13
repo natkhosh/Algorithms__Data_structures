46748b71427e960198290aeff8b457f5c0e3b3f8
natkhosh <t9219788607@gmail.com>
2020-01-13 18:51:08 +0300

    g0 - сортировка пузырьками сделана!!!

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,4 +10,22 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
+	container
+
+	for i in range(len(container) - 1):
+		for j in range(len(container) - 1 - i):
+			if container[j] > container[j + 1]:
+				container[j], container[j + 1] = container[j + 1], container[j]
+
 	return container
+
+
+if __name__ == "__main__":
+	import random
+	N = 5
+	# shuffle_list = list(range(N))
+	shuffle_list = [1, 0, 4, 2, 3]
+	random.shuffle(shuffle_list)
+	print(shuffle_list)
+
+	print(sort(shuffle_list))
