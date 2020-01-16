e638e5d86d1d3b5d8bfd47725731f5832e1c014a
natkhosh <t9219788607@gmail.com>
2020-01-16 19:46:50 +0300

    g1 - сортировка слиянием сделана!!!

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -2,6 +2,30 @@ from typing import Hashable, List
 import networkx as nx
 
 
+def dfs_find(graph, src, dst, visied):
+	visied[src] = True
+	if src == dst:  # src - откуда начинаем смотреть, dst - куда мы хотим дойти
+		return True
+
+	for node in graph.adj[src]:
+		if not visied[node]:
+			if dfs_find(graph, node, dst, visited):
+				return True
+	return False
+
+
+def dfs_new(graph, src, visied):
+	visied[src] = True
+	# if src == dst:  # src - откуда начинаем смотреть, dst - куда мы хотим дойти
+	# 	return True
+
+	for node in graph.adj[src]:
+		if not visied[node]:
+			if dfs_find(graph, node, visited):
+				return True
+	return False
+
+
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	"""
 	Do an depth-first search and returns list of nodes in the visited order
@@ -12,3 +36,36 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
 	"""
 	print(g, start_node)
 	return list(g.nodes)
+
+
+
+if __name__ == "__main__":
+	graph = nx. Graph()
+	graph.add_nodes_from("ABCDEFG")
+	graph.add_edges_from(
+		[
+			("A", "B"),
+			("A", "C"),
+			("B", "D"),
+			("B", "E"),
+			("C", "F"),
+			("E", "G")
+		]
+	)
+
+	graph.add_node('Z')
+	graph.add_node('X')
+	src = "A"
+	dst = "X"
+
+	visited = {node: False for node in graph.nodes}
+	print(graph.nodes())
+	for node in graph.adj:
+		print(node, graph.adj[node])
+
+	print(dfs_find(graph, src, dst, visited))
+	print(visited)
+
+
+	# if any visited.values() :
+	# 	dfs_find(graph, src, dst, visited)
