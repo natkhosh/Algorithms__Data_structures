dcab5881d09da2d7b62454a96a3b96aeb1ca2131
natkhosh <t9219788607@gmail.com>
2020-01-19 14:17:56 +0300

    bo  + tests    --> DONE!!!

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -3,13 +3,29 @@ This module implements some functions based on linear search algo
 """
 from typing import Sequence
 
+import numpy as np
+
 
 def min_search(arr: Sequence) -> int:
 	"""
 	Function that find minimal element in array
-
 	:param arr: Array containing numbers
 	:return: index of first occurrence of minimal element in array
 	"""
-	print(arr)
-	return -1
+	min_elem = min(arr)
+	print('Array:', arr)
+	print('Min:', min_elem)
+	index = None
+	for i, elem in enumerate(arr):
+		if elem == min_elem:
+			index = i
+			return index
+	return index
+
+
+if __name__ == "__main__":
+	n = 10
+	arr = np.arange(n)
+	find_elem = np.random.choice(arr)
+	np.random.shuffle(arr)
+	print('Index:', min_search(arr))
