10abed125cd5288d28666e0c229067cbe13592ca
natkhosh <t9219788607@gmail.com>
2020-01-19 16:25:19 +0300

    b2 - сделано только е*х
    ! sin*x - сдалать!!!

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,25 +2,49 @@
 Taylor series
 """
 from typing import Union
+import math
 
 
-def ex(x: Union[int, float]) -> float:
+def ex(x: Union[int, float], delta=0.0001) -> float:
 	"""
 	Calculate value of e^x with Taylor series
-
 	:param x: x value
 	:return: e^x value
 	"""
-	print(x)
-	return 0
+	if x == 0:
+		return 1
+	else:
+		result_sum = 1
+		n = 1
+		while True:
+			result = (x ** n) / math.factorial(n)
+			if result < delta:
+				return result_sum
+			else:
+				result_sum += result
+				n += 1
 
 
-def sinx(x: Union[int, float]) -> float:
+def sinx(x: Union[int, float], delta=0.0001) -> float:
 	"""
 	Calculate sin(x) with Taylor series
-
 	:param x: x value
 	:return: sin(x) value
 	"""
-	print(x)
-	return 0
+	if x == 0:
+		return 0
+	else:
+		result_sin = 1
+		n = 1
+		while True:
+			result_ = ((-1) ** n) * ((x ** (2 * n + 1)) / (math.factorial(2 * n + 1)))
+			if abs(result_) < result_sin - delta:
+				return result_ #result_sin
+			else:
+				#result_sin += result_
+				n += 1
+
+
+if __name__ == "__main__":
+	#print(ex(1.55433))
+	print(sinx(1.55433))
