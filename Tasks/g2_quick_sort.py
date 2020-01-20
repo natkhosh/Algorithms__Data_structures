d38654dea86963b6b24ebb6a2050c8c1b19bdfb9
natkhosh <t9219788607@gmail.com>
2020-01-20 20:58:48 +0300

    g2_quick_sort + test --> DONE!!!

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -10,4 +10,25 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
-	return container
+	less = []
+	equal = []
+	greater = []
+
+	if len(container) > 1:
+		pivot = container[0]
+		for x in container:
+			if x < pivot:
+				less.append(x)
+			if x == pivot:
+				equal.append(x)
+			if x > pivot:
+				greater.append(x)
+		return sort(less) + equal + sort(greater)
+	else:
+		return container
+
+
+if __name__ == '__main__':
+	array = [12, 4, 5, 6, 7, 3, 1, 15]
+
+	print(sort(array))
